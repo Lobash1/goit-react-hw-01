ff68234569255e06573d3645e7ffe9a44e7b893b
Nastya <lobash.n@gmail.com>
2025-02-20 15:20:20 +0200

    finish task 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "modern-normalize";
+
 import "./index.css";
-import App from "./components/App.jsx";
+import { App } from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
